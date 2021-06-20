cf2a7515431545f79f29d44c27356a940402e494
AdvikH <81264729+AdvikH@users.noreply.github.com>
2021-06-20 15:09:15 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,10 @@
+var first_name = "Advik";
+console.log(first_name);
+var last_name = "Harish";
+console.log(last_name);
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 10;
+var b = 5;
+var add = a + b;
+console.log(add);
